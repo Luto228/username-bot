c369142eb5d252a8aceb143c66a1cd141b1706d4
Luto228 <megapupsik909@gmail.com>
2026-08-31 20:44:04 +0400

    feat: The greeting and explanation of the bot's purpose became clearer; a callback.message was added (not yet completed).

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from email import message
 import os
 import asyncio
-from aiogram import Bot, Dispatcher
+from aiogram import Bot, Dispatcher, F
 from aiogram.filters import Command, CommandStart
-from aiogram.types import Message
+from aiogram.types import Message, CallbackQuery
 from dotenv import load_dotenv
 from Inline_keyboard import category_inline_keyboard 
+from start_find_username import start_find_username
 
 load_dotenv()
 
@@ -17,13 +18,18 @@ dp = Dispatcher()
 
 @dp.message(CommandStart())
 async def command_start(message: Message):
-    await message.answer("Hello! I will help you find a really good username!")
+    await message.answer("Hello! I will help you find a really good username! \n To start the process, please enter the command /username")
 @dp.message(Command("username"))
 async def command_username(message: Message):
     await message.answer("Do you want default categories or custom categories?")
-    await message.answer("Default categories: \n 8(minimal length), \n Both(noun and verb), \n yes(use AI), \n 2(minimal rarity)")
+    await message.answer("Default categories: \n 8(minimal length), \n yes(nouns), yes(verbs), yes(adjectives) or just y, y, y, \n yes(use AI), \n 2(minimal rarity)")
     await message.answer("Choose an option:", reply_markup=await category_inline_keyboard())
 
+@dp.callback_query(F.data == "default_categories")
+async def def_categories(callback: CallbackQuery):
+    await callback.answer("default")
+    await callback.message.edit_text("Great! wait until i generate username from default categories.")
+    found_username = start_find_username(8, "y", "y", "y", "y", 2)
 async def main():
     await dp.start_polling(bot)
 
