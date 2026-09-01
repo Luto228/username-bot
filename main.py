2eff706a6b7fe53f504af609c2fb22f3852f9d76
Luto228 <megapupsik909@gmail.com>
2026-09-01 15:04:51 +0400

    The default selection in the bot is now working. A custom option will be added soon as well.

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-from email import message
 import os
 import asyncio
 from aiogram import Bot, Dispatcher, F
@@ -6,7 +5,7 @@ from aiogram.filters import Command, CommandStart
 from aiogram.types import Message, CallbackQuery
 from dotenv import load_dotenv
 from Inline_keyboard import category_inline_keyboard 
-from start_find_username import start_find_username
+from start_find_username import find_username
 
 load_dotenv()
 
@@ -29,7 +28,13 @@ async def command_username(message: Message):
 async def def_categories(callback: CallbackQuery):
     await callback.answer("default")
     await callback.message.edit_text("Great! wait until i generate username from default categories.")
-    found_username = start_find_username(8, "y", "y", "y", "y", 2)
+    found_username = await find_username(8, "y", "y", "y", "y", 2)
+    if found_username is None: await callback.message.answer("You wrote uncorrect categories")
+    await callback.message.answer(f"Found username: {found_username}")
+@dp.callback_query(F.data == "custom_categories")
+async def custom_categories(callback: CallbackQuery):
+    await callback.answer("custom")
+    await callback.message.edit_text("Please enter your custom categories in the following format: \n max_length, noun, verb, adjectives, use_ai, minimal_rarity \n Example: 10, yes, no, yes, yes, 3")
 async def main():
     await dp.start_polling(bot)
 
